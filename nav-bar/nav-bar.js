1554a4f494065fc07c48a85e194ba266a0c96e95
unknown <polianabadio@gmail.com>
2023-05-24 18:41:44 -0300

    mais endereços 2

--- nav-bar/nav-bar.js
+++ nav-bar/nav-bar.js
@@ -1,8 +1,7 @@
-fetch('nav-bar/nav-bar.html')
+fetch('./nav-bar/nav-bar.html')
      .then(response => response.text())
      .then(data => {
         const navbar = document.createElement('nav');
         navbar.innerHTML = data;
         document.body.insertBefore(navbar, document.body.firstChild);
   });
-
