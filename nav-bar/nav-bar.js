bd788133ea209c39b79c656ffc80550bda9e59d0
unknown <polianabadio@gmail.com>
2023-05-24 13:38:56 -0300

    first commit

---
+++ nav-bar/nav-bar.js
@@ -0,0 +1,8 @@
+fetch('nav-bar/nav-bar.html')
+     .then(response => response.text())
+     .then(data => {
+        const navbar = document.createElement('nav');
+        navbar.innerHTML = data;
+        document.body.insertBefore(navbar, document.body.firstChild);
+  });
+
